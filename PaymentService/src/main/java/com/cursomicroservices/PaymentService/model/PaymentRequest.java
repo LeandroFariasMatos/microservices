eae176d492c084798597369d7c1c91f775426185
Leandro Matos <68133954+LeandroFariasMatos@users.noreply.github.com>
2023-03-15 19:59:55 -0300

    Update PaymentRequest.java

--- PaymentService/src/main/java/com/cursomicroservices/PaymentService/model/PaymentRequest.java
+++ PaymentService/src/main/java/com/cursomicroservices/PaymentService/model/PaymentRequest.java
@@ -12,6 +12,5 @@ import lombok.NoArgsConstructor;
 public class PaymentRequest {
     private long orderId;
     private long amount;
-    private String referenceNumber;
     private PaymentMode paymentMode;
 }
